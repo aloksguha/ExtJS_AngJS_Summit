41aa6a4186588ff800c0d4e0ea5f67e43bf1aa1b
Alok Guha <alok.guha@synerzip.com>
2014-04-10 16:44:17 +0530

    ext path changed

--- Training/Topics/grid/example1/simplegrid.js
+++ Training/Topics/grid/example1/simplegrid.js
@@ -1,10 +1,45 @@
-//The onReady function is what we use to make our code wait until the document is ready.
 
-function createSimpleGrid(){
 
+
+
+
+
+
+function createSimpleGrid() {
+  Ext.create('Ext.data.Store', {
+    storeId: 'synerzipStore',
+    fields: ['name', 'email', 'phone', 'doj'],
+    data: {'employees': [
+      { 'name': 'Pankaj', "email": "pankaj@synerzip.com", "phone": "234-234-5645", "doj": 'asd' },
+      { 'name': 'Vinay', "email": "vinay@synerzip.com", "phone": "234-294-5645", "doj": 'asd' },
+      { 'name': 'Nidhi', "email": "nidhi@synerzip.com", "phone": "234-134-5645", "doj": 'asd'  },
+      { 'name': 'Wasim', "email": "wasim@synerzip.com", "phone": "234-234-5645", "doj": 'asd'  }
+    ]},
+    proxy: {
+      type: 'memory',
+      reader: {
+        type: 'json',
+        root: 'employees'
+      }
+    }
+  });
+
+  Ext.create('Ext.grid.Panel', {
+    title: 'Synerzip',
+    store: Ext.data.StoreManager.lookup('synerzipStore'),
+    columns: [
+      { text: 'Name', dataIndex: 'name' },
+      { text: 'Email', dataIndex: 'email', flex: 1 },
+      { text: 'Phone', dataIndex: 'phone' },
+      { text: 'Date Of Joining', dataIndex: 'doj' }
+    ],
+    height: 200,
+    width: 400,
+    renderTo: Ext.getBody()
+  });
 }
 
-
+//The onReady function is what we use to make our code wait until the document is ready.
 Ext.onReady(function () {
   createSimpleGrid();
 });
