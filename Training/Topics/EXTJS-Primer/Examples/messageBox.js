b6cade50bc1731f5326f0d62fd77ecb3ec88466e
Alok Guha <alok.guha@synerzip.com>
2014-04-10 14:45:15 +0530

    ext libs

--- Training/Topics/EXTJS-Primer/Examples/messageBox.js
+++ Training/Topics/EXTJS-Primer/Examples/messageBox.js
@@ -9,16 +9,4 @@ Ext.onReady(function () {
      //Ext.get('result').setHTML(result);
 	});
 
-//
-//	Ext.Msg.show({
-//		title: 'Synerzip ExtJS Summit',
-//		msg: 'Alok Guha',
-//		width: 300,
-//		buttons: Ext.Msg.YES,
-//		multiline: true,
-//		fn: function(){Ext.get('result').setHTML(result);},
-//		icon: Ext.window.MessageBox.INFO
-//	});
-
-
 });
---
+++ Training/Topics/grid/example1/simplegrid.js
@@ -0,0 +1,10 @@
+//The onReady function is what we use to make our code wait until the document is ready.
+
+function createSimpleGrid(){
+
+}
+
+
+Ext.onReady(function () {
+  createSimpleGrid();
+});
